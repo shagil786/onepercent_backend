b35762ceafab28d392fec6ce8911b8f238527a3c
shagil786 <shagil.nizami@fincity.com>
2024-01-30 02:32:56 +0530

    Update aws.js

--- helpers/aws.js
+++ helpers/aws.js
@@ -1,9 +1,9 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
 AWS.config.update({
-    accessKeyId: 'AKIA4G7SZ7VF5PH3SL63',
-    secretAccessKey: '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',
-    region: 'eu-north-1'
+  accessKeyId: "Access Key",
+  secretAccessKey: "Secreat Key",
+  region: "eu-north-1",
 });
 
 const s3 = new AWS.S3();
